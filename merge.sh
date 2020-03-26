c2944aeeb584988822230b09e801c4d35a9a40b2
Bruno Oberle <b.oberle@zoho.eu>
2020-03-26 12:48:26 +0100

    fixed merge.sh

--- merge.sh
+++ merge.sh
@@ -1,6 +1,15 @@
-for f in $1/*.mp4:
+if [ "$1" == '' ];
+then
+   path="."
+else
+   path=$1
+fi
+
+
+for f in $path/*.mp4
 do
    fn=$(basename -s .mp4 $f)
+   #echo $fn-merged.mp4
    ffmpeg -i $fn.mp4 -i $fn.mp3 -vcodec copy -acodec copy $fn-merged.mp4
 done
 
